c177fdce9eab9a2134644f308f17defb7a8f68cb
Kang Minyoung <kangttam@gmail.com>
2025-07-08 14:41:43 +0900

    [ADD] reset test

--- Assets/1. Data Structure/2. Scripts/StudyDictionary.cs
+++ Assets/1. Data Structure/2. Scripts/StudyDictionary.cs
@@ -1,43 +1,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class PersonData
+{
+    public int age;
+    public string name;
+    public float height;
+    public float weight;
+
+    public PersonData(int _age, string _name, float _height, float _weight)
+    {
+        age = _age;
+        name = _name;
+        height = _height;
+        weight = _weight;
+    }
+}
+
 public class StudyDictionary : MonoBehaviour
 {
-    public Dictionary<string, int> persons = new Dictionary<string, int>();
+    public Dictionary<string, PersonData> persons = new Dictionary<string, PersonData>();
 
     private void Start()
     {
         // Dictionary 데이터 추가
-        persons.Add("철수", 10);
-        persons.Add("영희", 15);
-        persons.Add("동수", 17);
+        persons.Add("철수", new PersonData(10, "철수", 150f, 30f));
+        persons.Add("영희", new PersonData(10, "영희", 150f, 30f));
+        persons.Add("동수", new PersonData(10, "동수", 150f, 30f));
+
+        Debug.Log(persons["철수"].age);
+        Debug.Log(persons["철수"].name);
+        Debug.Log(persons["철수"].height);
+        Debug.Log(persons["철수"].weight);
 
         //key 로 데이터 받아오기
-        int age = persons["동수"];
-        Debug.Log($"철수의 나이는 {age}");
+        //int age = persons["동수"];
+        //Debug.Log($"철수의 나이는 {age}");
 
         // value 로 데이터 받아오기
         //string name = persons[15];
         // 중복이 허용되어, 판단 불가
-
-        foreach (var person in persons)
-        {
-            if(person.Value == 15)
-            {
-                Debug.Log($"15세는 {person.Key}");
-            }
-
-            Debug.Log($"{person.Key}의 나이는 {person.Value}");
-
-            if (persons.ContainsKey("철수"))
-            {
-                Debug.Log("딕셔너리 내 철수 확인");
-            }
-            if (persons.ContainsValue(17))
-            {
-                Debug.Log("딕셔너리 내 17살 존재");
-            }
-        }
     }
 }
 
