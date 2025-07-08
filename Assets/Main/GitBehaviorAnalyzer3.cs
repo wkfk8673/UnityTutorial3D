678bddc8764370cfb37134c0a3f6100edca46400
Kang Minyoung <kangttam@gmail.com>
2025-07-08 11:31:30 +0900

    2

--- Assets/Main/GitBehaviorAnalyzer3.cs
+++ Assets/Main/GitBehaviorAnalyzer3.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class 1 : MonoBehaviour
+public class GitBehaviorAnalyzer3 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
